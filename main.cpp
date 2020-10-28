22e94f78ae3d24c473be7a46ed3b1335e2c417ab
Isaac Emmanuel Muñoz Flores <isaac.munoz8648@alumnos.udg.mx>
2020-10-27 22:25:16 -0600

    Actividad 12

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "arreglo.h"
+
+using namespace std;
+
+void modificar(int *a)
+{
+    *a = *a + 1;
+}
+
+int main()
+{   
+    Arreglo arreglo;
+    arreglo.insertar_final(8);
+    arreglo.insertar_inicio(2);
+
+    for (size_t i = 0; i < arreglo.size(); i++)
+    {
+        cout << arreglo[i] << " ";
+    }
+    
+
+    //int *b = nullptr;
+    //b = new int[100];
+    //cout << b << " " << *b << endl;
+    //delete[] b;
+    //int a = 12;
+
+    //cout << a << endl;
+    //cout << &a << endl;
+    //modificar(&a);
+    //cout << a << endl;
+
+    return 0;
+}
